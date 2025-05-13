17d3b8842e0e3e7970aed4dccbab4f7ea062a12e
Raghunathan <srinirag@cc-login2.campuscluster.illinois.edu>
2025-05-12 19:42:36 -0500

    batch script

---
+++ submit_chains.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=caps-all
+#SBATCH --output=batch_jobs/job.o%j
+#SBATCH --cpus-per-task=40
+#SBATCH --exclude=ccc0240,ccc0242
+#SBATCH --nodes=3
+#SBATCH --time=24:0:00
+#SBATCH --mem=20G
+export SHELL=bash
+
+#example
+#coabya-run yamls/so_baseline_sampler_plus_desidr2boa.yaml
+cobaya-run $1
