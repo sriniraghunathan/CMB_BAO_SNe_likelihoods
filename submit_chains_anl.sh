6a038e3862051f304686a97036067c3ee958ef73
sriniraghuna <sriniraghuna@gmail.com>
2025-05-13 00:09:57 -0500

    minor

--- submit_chains_anl.sh
+++ submit_chains_anl.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --partition=SPT3G
 #SBATCH --output=batch_jobs/job.o%j
-#SBATCH --cpus-per-task=30
+#SBATCH --cpus-per-task=50
 ###SBATCH --exclude=ccc0240,ccc0242
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --time=24:0:00
 #SBATCH --mem=20G
 export SHELL=bash
