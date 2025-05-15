7fc6def494dbd8c57a2dea8cfee441067b3c4848
sriniraghuna <sriniraghuna@gmail.com>
2025-05-15 12:27:39 -0500

    submit script

--- submit_chains.sh
+++ submit_chains.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --partition=caps-all
 #SBATCH --output=batch_jobs/job.o%j
-#SBATCH --cpus-per-task=$3 #20
+#SBATCH --cpus-per-task=20
 #SBATCH --exclude=ccc0240,ccc0242
-#SBATCH --nodes=$2 #4
+#SBATCH --nodes=4
 #SBATCH --time=24:0:00
 #SBATCH --mem=20G
 export SHELL=bash
